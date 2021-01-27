06388cdb8389f0ee87f4a6ec664d1c9f345c99d4
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-01-27 23:50:48 +0100

    Buycoins client

---
+++ buycoins/client.py
@@ -0,0 +1,27 @@
+from python_graphql_client import GraphqlClient
+from requests.auth import HTTPBasicAuth
+
+from buycoins.exceptions import QueryError
+
+class BuyCoinsClient:
+    def __init__(self, auth_key: str):
+        self.__endpoint = "https://backend.buycoins.tech/api/graphql"
+        self.__username = ""
+        self.__password = ""
+        self.auth_key = auth_key
+
+    def _split_auth_key(self):
+        self.__username, self.__password = self.auth_key.split(":")
+
+    def _initiate_client(self):
+        self._split_auth_key()
+        self.auth = HTTPBasicAuth(self.__username, self.__password)
+        self.client = GraphqlClient(self.__endpoint)
+        return self.client
+
+    def _execute_request(self, query: str, variables: dict= {}):
+        if not query or query == "":
+            raise QueryError("Invalid query passed!")
+
+        self._initiate_client()
+        return self.client.execute(query=query, variables=variables, auth=self.auth)

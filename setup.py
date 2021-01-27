677d7877bffc65f3943015d25e988be057a93fbb
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-01-27 23:50:16 +0100

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='buycoins',
+    version='0.0.1',
+    description='Python SDK for the BuyCoin\'s API',
+    url='',
+    author_email=['youngestdev@gmail.com'],
+    license = '',
+    install_requires=['python-graphql-client'],
+    packages=['buycoins']
+)

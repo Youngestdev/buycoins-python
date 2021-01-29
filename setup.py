eeb876c1a9df9b8162657715ef2312285a34b113
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-01-29 01:17:33 +0100

    Update: Updated setup file.
    Improved the setup file for the package.

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
 
 setup(
     name='buycoins',
-    version='0.0.1',
+    version='0.1.1',
     description='Python SDK for the BuyCoin\'s API',
-    url='',
-    author_email=['youngestdev@gmail.com'],
-    license = '',
-    install_requires=['python-graphql-client'],
-    packages=['buycoins']
+    url='https://github.com/Youngestdev/BuyCoins-Python',
+    author="Abdulazeez Abdulazeez Adeshina",
+    author_email='youngestdev@gmail.com',
+    license = 'LICENSE',
+    install_requires=['python-graphql-client', 'requests'],
+    packages=find_packages(),
+    python_requires='>=3.6'
 )

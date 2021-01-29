49fedbcb8c9e22344c8319fd1cb78025a859076f
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-01-29 01:15:49 +0100

    Update: Added more exception classes.
    Added the following classes:
    
    - P2PError: Responsible for errors in the P2P class.
    - AccountError: Responsible for error in the NGNT `createDepositAccount` method.

--- buycoins/exceptions/exceptions.py
+++ buycoins/exceptions/exceptions.py
@@ -10,3 +10,30 @@ class QueryError(Exception):
             return 'QueryError, {0} '.format(self.message)
         else:
             return 'A QueryError has been raised'
+
+class P2PError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'P2PError, {0} '.format(self.message)
+        else:
+            return 'A P2P has been raised'
+
+class AccountError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'AccountError, {0} '.format(self.message)
+        else:
+            return 'An AccountError has been raised'
+

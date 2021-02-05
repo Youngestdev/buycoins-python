99409ee65c74c2c250bd040a1e3da8988d46bb0c
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2021-02-05 23:20:52 +0100

    major fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="buycoins-python",
-    version="0.4",
+    version="0.5",
     description="Python SDK for the BuyCoin\"s API",
     long_description=README,
     long_description_content_type="text/markdown",
